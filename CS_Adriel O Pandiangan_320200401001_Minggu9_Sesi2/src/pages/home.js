927821cc2214c3442476ddd3e7ae762eb5ea76ba
adrielop <92299780+adrielop@users.noreply.github.com>
2022-12-04 17:34:53 +0700

    Update home.js

--- CS_Adriel O Pandiangan_320200401001_Minggu9_Sesi2/src/pages/home.js
+++ CS_Adriel O Pandiangan_320200401001_Minggu9_Sesi2/src/pages/home.js
@@ -48,7 +48,7 @@ const Home = () => {
     // set isLogin menjadi true. bila tidak, redirect ke halaman login
     const verify = async () => {
       try {
-        const response = await axios.post(`https://modul17adrielomarpadupandiangan-production.up.railway.app/verify`, {
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/verify`, {
           token: localStorage.getItem("token"),
         });
         if (response.status == 200) {
